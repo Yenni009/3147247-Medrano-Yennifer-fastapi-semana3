53b21ced48c69fd2e55619bc46548d1058211778
Yenni009 <200133232+Yenni009@users.noreply.github.com>
2025-09-03 21:08:28 -0500

    Delete mi-api-organizada/ejercicio-biblioteca/app/tests/test_books.py

--- mi-api-organizada/ejercicio-biblioteca/app/tests/test_books.py
+++
@@ -1,8 +0,0 @@
-def test_create_book_success():
-    assert True  # luego agregamos tests con requests o testclient
-
-def test_create_book_duplicate_isbn():
-    assert True
-
-def test_get_book_not_found():
-    assert True
