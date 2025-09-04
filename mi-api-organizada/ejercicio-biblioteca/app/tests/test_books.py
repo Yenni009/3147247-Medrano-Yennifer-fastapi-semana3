4bb2ac45dd9948473849638e8bb0dcd73e098f89
Yenni009 <200133232+Yenni009@users.noreply.github.com>
2025-09-03 21:06:06 -0500

    Create test_books.py

---
+++ mi-api-organizada/ejercicio-biblioteca/app/tests/test_books.py
@@ -0,0 +1,8 @@
+def test_create_book_success():
+    assert True  # luego agregamos tests con requests o testclient
+
+def test_create_book_duplicate_isbn():
+    assert True
+
+def test_get_book_not_found():
+    assert True
